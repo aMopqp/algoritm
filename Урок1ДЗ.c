105c401d1b21918b21adabf68d29d25caab8a02e
aMopqp <aMopqp@yandex.ru>
2019-07-05 19:17:37 +0400

    Исправил после прочтения коментария

--- Урок1ДЗ.c
+++ Урок1ДЗ.c
@@ -2,12 +2,13 @@
 #include <math.h>
 
 
-double IndexMass(int m, int h)   //workout 1
+
+double IndexMass(int m, int h)   //Задание 1
 {
 	return m / (h*h);
 }
 
-void average()  // workout on lecture
+void average()  // Задание с лекции
 {
 	printf("\n write numbers. Exit =-1 \n");
 	int counter = -1;
@@ -22,44 +23,57 @@ void average()  // workout on lecture
 	getchar();
 }
 
-void Choose(int old) //workout  3 
+void Choose(int old) //Задание 3 
 {
 	if ((old % 10 == 1) && (old != 11))
-		printf("%d \ngod", old);
+		printf("%d \nгод", old);
 	else if ((old % 10 > 1) && (old % 10 < 5) && (old / 10 != 1))
-		printf("%d \ngodaа", old);
-	else printf("%d \nlet", old);
+		printf("%d \nгода", old);
+	else printf("%d \nлет", old);
 }
 
-void BinaryEquation(int a, int b, int c)  //workout  2
+struct  root
 {
+	double x1;
+	double x2;
+};
+
+struct root BinaryEquation(int a, int b, int c)  //Задание 2
+{
+	struct root Honda;
 	double D = b*b - 4 * a*c;
 	if (D > 0) {
-		printf("\nx1 = %0.2f", ((0 - b) + sqrt(D)) / 2 * a);
-		printf("\nx2 = %0.2f", ((0 - b) - sqrt(D)) / 2 * a);
+		Honda.x1 = (0 - b) + sqrt(D) / 2 * a;
+		Honda.x2= (0 - b) - sqrt(D) / 2 * a;
 	}
 	if (D == 0)
 	{
-		printf("\nx1 = %0.2f", ((0 - b) + sqrt(D)) / 2 * a);
+		Honda.x1 = (0 - b) + sqrt(D) / 2 * a;
+		Honda.x2 = acos(-1);
 	}
 	if (D<0)
 	{
-		printf("\nno root");
+		Honda.x1 = acos(-1);
+		Honda.x2 = acos(-1);
 	}
+	return Honda;
 }
 
-void Chess(int x1, int x2, int y1, int y2)    //workout  4
+void Chess(int x1, int x2, int y1, int y2)    //Задание 4
 {
 	printf((x1 + x2 + y1 + y2) % 2 != 0 ? "\n equal" : "\n no equal");
 }
 
-void autoMorf10k()               //workout  5
+void autoMorf10k(int Max)               //workout  5
 {
+	int divider = 100;
 	int i = 1;
-	while (i<100)
+	while (i<sqrt(Max))
 	{
-		if (i*i % 100 == i) printf("\n%d^2=%d",i ,i*i);
+
+		if (i*i % divider == i) printf("\n%d^2=%d", i, i*i);
 		i++;
+		if (!(i<divider)) divider *= 10;
 	}
 }
 
@@ -69,12 +83,14 @@ int main(int argc, char *argv[])
 	printf("Индекс массы тела %0.2f", a);
 
 	Choose(25);
-
-	BinaryEquation(3, 6, 9);
-
+	struct root mazda;
+	mazda = BinaryEquation(3, 6, 9);
+	printf("Binary equals have root\n");
+	if (mazda.x1 != acos(-1)) printf("x1=%0.2f\n", mazda.x1);
+	if (mazda.x2 != acos(-1)) printf("x1=%0.2f\n", mazda.x2);
 	Chess(1, 1, 5, 6);
 
-	autoMorf10k();
+	autoMorf10k(10000);
 
 	average();
 	return 0;
